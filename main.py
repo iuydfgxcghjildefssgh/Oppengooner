2721424fe1e9488e745ddc71e33e13d252a16105
25lnoy <25lnoy@mlmc.vic.edu.au>
2025-09-02 21:20:23 +1000

    tried to scrape images

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import tkinter as tk
+from bs4 import BeautifulSoup
+import requests
 
+def on_enter_btn_clicked():
+    url = URL_Input.get()   # get user input here
+    requestURL(url)         # pass it into requestURL()
+    SoupUse(soup)
+
+def requestURL(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+def SoupUse(soup):
+    images = soup.find_all('gallery_thumb')
+
+
+# Tkinter setup
 window = tk.Tk()
 window.geometry("1000x600")
 
-
-def on_enter_btn_clicked():
-    Inputted_URL = URL_Input.get()
-    print(Inputted_URL)
-
-Frame1 = tk.Frame()
-Frame2 = tk.Frame()
+Frame1 = tk.Frame(window)
+Frame2 = tk.Frame(window)
 
 Insert_URL = tk.Label(master=Frame1, text="Insert URL")
 Insert_URL.pack()
-URL_Input = tk.Entry(master=Frame2, fg="White", bg="Black", width=70)
-URL_Input.pack()
-enter_btn = tk.Button(master=Frame2, text="Enter", command=on_enter_btn_clicked)
-enter.pack()
 
+URL_Input = tk.Entry(master=Frame2, fg="white", bg="black", width=70)
+URL_Input.pack()
+
+enter_btn = tk.Button(master=Frame2, text="Enter", command=on_enter_btn_clicked)
+enter_btn.pack()
 
 Frame1.pack()
 Frame2.pack()
-
 
 window.mainloop()
